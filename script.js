0aec5ba1202c5bb63402db4a1b6c978541e8b2af
laxmis-prog <laxmisanuu5@gmail.com>
2026-01-16 19:54:39 +0200

    Add a third object with properties and values.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const allSongs = [
     duration: "4:25",
     src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
   },
-  
+
 {
   id: 1,
 title: "Can't Stay Down",
@@ -22,5 +22,12 @@ artist: "Quincy Larson",
 duration: "4:15",
 src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
 },
+{
+  id: 2,
+title: "Still Learning",
+artist: "Quincy Larson",
+duration: "3:51",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+}
 
 ];
